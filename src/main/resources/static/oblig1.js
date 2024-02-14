f65cab6b0804cdeb0679fab2bd4d81a841fa9a30
Abdisharif107 <143189472+Abdisharif107@users.noreply.github.com>
2024-02-14 19:06:00 +0100

    forsetta med obligen og gjort endringer

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -1,58 +1,80 @@
 const billetter = [];
-function leggTilBilett(billetter){
 
 }
 
-
-function validerAntall(Antall){
-    let regx = /^[0-9]{8}$/;
-    if(regx.test (Antall)){
-        $("#feilAntall").html("");
-        return true;
+function kjopBillett(){
+    if (document.getElementById("filmError").innerHTML === ""){
     }
-    else {
-        $("#feilAntall").html("Velg antall mellom 0-9");
-        return false;
+    if (document.getElementById("feilNavn").innerHTML !== "" ||
+        document.getElementById("feilEtternavn").innerHTML !== "" ||
+        document.getElementById("feilEpost").innerHTML !== "" ||
+        document.getElementById("feilTelefonnr").innerHTML){
+        return;
     }
+    let altAvBilletten = billetter.length; // legger til antall billeter som er allerede skrevet
+    const bilett = {
+        film: document.getElementById("film").value,
+        antall: document.getElementById("antall").value,
+        navn: document.getElementById("navn").value,
+        etternavn: document.getElementById("etternavn").value,
+        telefonnr: document.getElementById("telefonnr").value,
+        epost: document.getElementById("epost").value
+    };
+    billetter.push(bilett);
+    for (let i = altAvBilletten; i < billetter.length; i++){
+        let textParagraph = document.createElement("p");
+        textParagraph.textContent = "Film: " + billetter[i].film + ", Antall: " +
+            billetter[i].antall + ", Navn: " + billetter[i].navn + " " + billetter[i].etternavn +
+            ", Telefonnr: " + billetter[i].telefonnr + ", Epost: " + billetter[i].epost;
+        document.getElementById("billetter").appendChild(textParagraph);
+        document.getElementById("billetter").appendChild(document.createElement("br"));
+    }
+    document.getElementById("film").value = "";
+    document.getElementById("antall").value = "";
+    document.getElementById("navn").value = "";
+    document.getElementById("etternavn").value = "";
+    document.getElementById("telefonnr").value = "";
+    document.getElementById("epost").value = "";
 }
+   if (document.getElementById("antall").value === ""){
+       document.getElementById("feilAntall").innerText = "Må skrive inn antall"
+   }
 
-function valideringNavn(navn){
-    let regx = /^[A-Z-zæøå. -]{2.50}$/;
-    if (regx.test(navn)){
-        $("#FeilNavn").html("");
-        return true;
-    }
-    else {
-        $("#feilNavn").html("Skriv riktig navn og bruk store og små bokstaver");
-        return false;
-    }
-}
+   if(document.getElementById("navn").value === ""){
+       document.getElementById("feilNavn").innerText = "Må skrive inn navn"
+   }
 
-function validerTelefonnr(telefonnr){
+   if (document.getElementById("etternavn").value === ""){
+       document.getElementById("feilEtternavn").innerText = "Må skrive inn etternavn"
+   }
+
+   if (document.getElementById("telefonnr").value === ""){
+       document.getElementById("feilTelefonnr").innerText = "Må skrive inn telefonnr"
+   }
+
+   if (document.getElementById("epost").value === ""){
+       document.getElementById("feilEpost").innerText = "Må skrive inn epost"
+   }
+
+function valTelefonnr(telefonnr){
     let regx =/^[0-9]{8}$/;
     if (regx.test(telefonnr)){
-        $("#feilTelefonnr").html("")
+        document.getElementById("feilTelefonnr").innerHTML = "";
         return true;
-    }
-    else {
-        $("#feilTelefonnr").html("Bruk tall mellom 0-9");
-        return false;
-    }
-}
-function validerEpost(epost){
-    let regx = /^[a-zA-Z0-9. -]+\. [a-zA-Z]{2,4}$/;
-    if (regx.test(epost)){
-        $("#feilEpost").html("")
-        return true;
-    }
-    else{
-        $("#feilEpost").html("Feil med epost valderingen");
-        return false;
     }
 }
 
-function kjopBilett(){
-    const film = $("#film")
-    const antall = $("#antall")
-    const navn = $("#feilnavn")
+function valEpost(epost){
+    let regx = /^[a-zA-Z0-9. -]+\. [a-zA-Z]{2,4}$/;
+    if (regx.test(epost)){
+        document.getElementById("feilEpost").innerHTML = "";
+        return true;
+    }
+}
+
+function slettBillett(){
+        while (billetter.length !==0){
+            billetter.pop();
+    }
+    document.getElementById("billeter").innerHTML="";
 }
